4fcc039f56e9d8dafc6bd64c082ffeac687cee15
Elijah Flores <140471894+elnflores@users.noreply.github.com>
2024-06-03 15:12:30 -0500

    Time: 1 ms (34.68%), Space: 41.7 MB (24.29%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,9 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] arr = s.split(" ", 0);
+        if (arr.length == 0) {
+            return 0;
+        }
         return arr[arr.length - 1].length();
     }
 }
