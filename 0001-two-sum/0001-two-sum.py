a6e4deb2a8308fb01418f9d73b04d44aa6c27cb4
Elijah Flores <140471894+elnflores@users.noreply.github.com>
2025-03-21 17:28:43 -0500

    Time: 3 ms (51.12%), Space: 19.1 MB (11.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-
-        # loop through the array of ints
+        vals = {} # val : index
         for i in range(len(nums)):
-            # take our target, and subtract the current int
-            cmp = target - nums[i]
-            if cmp in hashmap:
-                # if we find a matching int, we can return their indices
-                return [i, hashmap[cmp]]
-            # if not, add to hashmap in case we need it later
-            hashmap[nums[i]] = i
+            val = nums[i]
+
+            if vals.get(val) == None:
+                vals[val] = i
+
+            diff = target - val
+            if vals.get(diff) != None and vals.get(diff) != i:
+                return [i, vals.get(diff)]
+
+
         
